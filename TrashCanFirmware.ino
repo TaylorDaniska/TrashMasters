fc69f8eeed807e22e675b41b57963a11d1f73751
Taylor Daniska <taylordaniska@gmail.com>
2020-03-10 23:27:24 -0400

    Add files via upload

--- TrashCanFirmware.ino
+++ TrashCanFirmware.ino
@@ -7,33 +7,53 @@
 //////////////////////////////////////////////////////////////////////
 #include <Servo.h>
 
+void openlid(int);
+int getspeed();
+
 Servo servo1;
 
-const int triggerpin = 8;
+const int triggerpin = 9;
 const int echopin = 10;
-const int servopin = 9;
+const int servopin = 11;
 int servoposition = 0;
 long duration;
 int distance;
+int minrotate = 0;
+int maxrotate = 90;
+int mindist = 10;
 
 void setup() {
   servo1.attach(servopin);
   pinMode(triggerpin, OUTPUT);
   pinMode(echopin, INPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
-  for (servoposition=0;servoposition<=180;servoposition++){ 
-    servo1.write(servoposition);              
-    delay(20);}
-  for (servoposition=180;servoposition>=0;servoposition++){ 
-    servo1.write(servoposition);              
-    delay(20);}
-  digitalWrite(triggerpin, LOW);
-  delayMicroseconds(2);
-  digitalWrite(triggerpin, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(triggerpin, LOW);
-  duration = pulseIn(echopin, HIGH);
-  distance= duration*0.034/2;
+  servo1.write(minrotate);
+    while (1){
+    distance = getspeed();
+Serial.print("Distance: ");
+Serial.println(distance);
+  if (distance<mindist)
+    openlid(5);
+  }
 }
+int getspeed(){
+    digitalWrite(triggerpin, LOW);
+    delayMicroseconds(2);
+    digitalWrite(triggerpin, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(triggerpin, LOW);
+    duration = pulseIn(echopin, HIGH);
+    distance= duration*0.034/2;
+    return distance;
+}
+
+void openlid(int openduration){
+  servo1.write(maxrotate);
+  delay(1000*openduration);
+  servo1.write(minrotate);
+}
+
+
